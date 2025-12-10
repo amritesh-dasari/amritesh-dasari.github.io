e089ea91ea5235f90e54941ada1a26a971057528
Amritesh Dasari <dmamritesh@gmail.com>
2025-12-10 21:02:26 +0530

    Update projects

--- src/app/Components/ProjectsSection.jsx
+++ src/app/Components/ProjectsSection.jsx
@@ -7,55 +7,69 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
     {
         id: 1,
+        title: "NOVA Teams Chatbot",
+        description: "NOVA is a Teams-based AI chatbot that uses Azure Document Intelligence, Teams SSO, and Graph API to analyze user-shared files and deliver structured insights directly within Teams.",
+        tag: ["All", "AI"],
+        gitUrl: "https://github.com/amritesh-dasari/",
+    },
+    {
+        id: 2,
+        title: "AURA Teams Chatbot",
+        description: "AURA is a Teams-based AI agent that uses Azure OpenAI and a RAG pipeline to search, retrieve, and deliver context-aware answers from SharePoint and OneDrive content.",
+        tag: ["All", "AI"],
+        gitUrl: "https://github.com/amritesh-dasari/",
+    },
+    {
+        id: 3,
         title: "Chat-Time",
         description: "Coded a real-time chat website using React, Node.js and Appwrite Cloud that implemented client-server subscription for real-time messaging and allowed message deletion based on message IDs.",
         tag: ["All", "Web"],
         gitUrl: "https://github.com/amritesh-dasari/Chat-Time",
     },
     {
-        id: 2,
+        id: 4,
         title: "FCC-HFTracer",
         description: "Developed a website visualization of publicly available FCC data between locations using indexing to optimize complex queries hosted on a GCP server and SQL instance.",
         tag: ["All", "Web"],
         gitUrl: "https://github.com/amritesh-dasari/FCC-HFTracer",
     },
     {
-        id: 3,
+        id: 5,
         title: "Distributed Machine Learning Cluster",
         description: "Implemented a distributed machine learning system with 10 nodes having integrated failure detection using a logsaver program and versioned file system with replication for efficient storage and maintenance.",
         tag: ["All", "System Design"],
         gitUrl: "https://github.com/amritesh-dasari/CS425-Distributed-Systems-UIUC/tree/main/cs425_mp4",
     },
     {
-        id: 4,
+        id: 6,
         title: "VTOP Extended",
         description: "Developed an Android application for Vellore Institute of Technology with authentication, clubs, events, faculty database and additional features like web view of campus portal, online quizzes using Flutter and Firebase.",
         tag: ["All", "Mobile"],
         gitUrl: "https://github.com/amritesh-dasari/VTOP-Extended",
     },
     {
-        id: 5,
+        id: 7,
         title: "BCFL: Blockchain based Federated Learning",
         description: "Researched improving security and reliability of Federated Learning by incorporating a blockchain-based distributed ledger for model update record-keeping and replacing the central server with Proof-of-Work validated miners.",
         tag: ["All", "System Design"],
         gitUrl: "https://gitlab.engr.illinois.edu/mdasari2/cs-525-research",
     },
     {
-        id: 6,
+        id: 8,
         title: "notsosimpletictactoe",
         description: "Mobile Tic-Tac-Toe game built with Flutter uses minimax algorithm for AI opponent to allow single player mode.",
         tag: ["All", "Mobile"],
         gitUrl: "https://github.com/amritesh-dasari/notsosimpletictactoe",
     },
     {
-        id: 7,
+        id: 9,
         title: "Diabetic Retinopathy Diagnosis",
         description: "Mobile App developed using TensorFlow Lite and Flutter that allows user to upload fundus images to check if a person is suffering from Diabetic Retinopathy. The Model has an accuracy of 91% based on publicly available datasets.",
         tag: ["All", "Mobile"],
         gitUrl: "https://github.com/amritesh-dasari/Diabetic-Retinopathy-Diagnosis",
     },
     {
-        id: 8,
+        id: 10,
         title: "Blog Application",
         description: "Backend REST API implementation for a Blog Application based on Spring Boot and MySQL.",
         tag: ["All", "Web"],
@@ -124,6 +138,11 @@ const ProjectsSection = () => {
                     onClick={handleTagChange}
                     name="All"
                     isSelected={tag === "All"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="AI"
+                    isSelected={tag === "AI"}
                 />
                 <ProjectTag
                     onClick={handleTagChange}
